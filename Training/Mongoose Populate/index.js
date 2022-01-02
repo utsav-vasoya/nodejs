d39e278101df4c6fd8b753ddffcbae79dec25f31
Utsav Vasoya <utsavvasoya89@gmail.com>
2022-01-02 11:39:34 +0530

    Mongoose Populate

--- Training/Mongoose Populate/index.js
+++ Training/Mongoose Populate/index.js
@@ -1,28 +1,14 @@
-const { response } = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 3000;
 mongoose.connect('mongodb://localhost:27017/populate',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    });
-
-const userSchema = new mongoose.Schema({
-    username: String
-})
-
-const postSchema = new mongoose.Schema({
-    title: String,
-    postedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
-})
-
-const User = mongoose.model('User', userSchema, 'users');
-const Post = mongoose.model('Post', postSchema, 'posts');
-
-
-Post.find()
-    .populate("postedBy")
-    .then(p => console.log(p))
-    .catch(error => console.log(error));
+    }, () => console.log("Database Connected"));
+app.use(express.json());
+app.use('/', require('./src/routes/user'))
+app.listen(port, () => {
+    console.log("Server Start");
+});
--- Training/Mongoose Populate/src/routes/user.js
+++ Training/Mongoose Populate/src/routes/user.js
@@ -1,64 +1,35 @@
-// const express = require('express');
-// const router = express.Router();
-// const User = require("../schema/user_schema");
+const express = require('express');
 const mongoose = require('mongoose');
-// router.post('/', (req, res) => {
-//     const newuser = new User({
-//         username: req.body.username,
-//         email: req.body.email,
-//         title: req.body.title
-//     })
-//     newuser.save()
-//         .then((result) => {
-//             res.send(result);
-//         }).catch((err) => console.log(err));
-// })
+const router = express.Router();
+const User = require("../schema/user_schema");
+const Post = require("../schema/post_schema");
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    email: String
+router.post('/user', (req, res) => {
+    const newuser = new User({
+        username: req.body.username
+    })
+    newuser.save()
+        .then((result) => {
+            res.send(result);
+        }).catch((err) => console.log(err));
 })
 
-const postSchema = new mongoose.Schema({
-    title: String,
-    postedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
+router.post('/post/:_id', (req, res) => {
+    const newuser = new Post({
+        title: req.body.title,
+        postedBy: req.params._id
+    })
+    newuser.save()
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => console.log(err));
 })
-const User = mongoose.model('User', userSchema);
-const Post = mongoose.model('Post', postSchema);
 
-// router.post('/', (req, res) => {
-//     const newuser = new User({
-//         username: req.body.username,
-//         email: req.body.email
-//     })
-//     newuser.save()
-//         .then((result) => {
-//             res.send(result);
-//         }).catch((err) => console.log(err));
-// })
+router.get('/', (req, res) => {
+    Post.find({})
+        .populate('postedBy', { username: 1, _id: 0 })
+        .then(p => res.send(p))
+        .catch(error => console.log(error));
+})
 
-// router.get('/', (req, res) => {
-Post.find()
-    // .populate('postedBy')
-    .then(p => console.log(p))
-    .catch(error => console.log(error));
-// User.find().populate('postedBy')
-//     .then((result) => {
-//         res.send(result);
-// })
-
-// router.get('/user', (req, res) => {
-//     User.find()
-
-//         .then(p => console.log(p))
-//         .catch(error => console.log(error));
-//     // User.find().populate('postedBy')
-//     //     .then((result) => {
-//     //         res.send(result);
-// })
-
-
-// module.exports = router;
+module.exports = router;
