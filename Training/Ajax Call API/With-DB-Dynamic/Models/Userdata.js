4847975855ced119e9e5d528fd50dcf1459d1cd8
Utsav Vasoya <utsavvasoya89@gmail.com>
2022-01-19 22:22:25 +0530

    AJAX

---
+++ Training/Ajax Call API/With-DB-Dynamic/Models/Userdata.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const userdata = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    }
+
+})
+
+
+
+module.exports = mongoose.model("User", userdata, "userdata")
